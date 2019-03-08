a8088c0b507726b031fc6abe4a130397ad1ead7e
ishantanu <shantanud106@gmail.com>
2019-03-08 16:03:22 +0530

    Typo fixes
    Signed-off-by: ishantanu <shantanud106@gmail.com>

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 target="./main.yml"
 rm "$target"
-echo "# Derived from ./contour" >> "$target"
+echo "# Derived from ./contour-config" >> "$target"
 echo "Creating main.yaml"
 
 for file in $(find ./contour-config -type f -name "*.yaml" | sort) ; do
